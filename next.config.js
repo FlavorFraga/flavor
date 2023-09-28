9020d38b4edd8b375c08a3a6f0acf25936cbe629
Yeison Garzon T. <garzontoroyeison@gmail.com>
2023-09-28 11:45:25 -0500

    pwa config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const prod = process.env.NODE_ENV === "production";
 
 const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: prod ? false : true,
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/app",
+  sw: "service-worker.js",
 });
 
 const KEYS_TO_OMIT = [
